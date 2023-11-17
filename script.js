5ab53947940c9199487836475d8f94cafbe8b9fe
Mohsina-Ahmed <122023111+Mohsina-Ahmed@users.noreply.github.com>
2023-11-17 19:24:08 +0000

    Adding reliability project

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
-const progressBarPercents = [90, 70, 70, 80, 70, 90];
+const progressBarPercents = [90, 70, 70, 80, 70, 70, 70];
 
 window.addEventListener("scroll", () => {
   mainFn();
